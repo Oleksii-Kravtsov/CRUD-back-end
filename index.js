9f4a2a06953b97fafb848d13b7e536c5ad14226b
Oleksii Kravtsov <bahbok1@gmail.com>
2022-01-23 17:12:05 -0500

    added all of the functionality to manipulating and retrieving information from the database.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const { response } = require('express')
+const { options } = require('pg/lib/defaults')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+app.use(bodyParser.json())
 app.listen(port, () => console.log(`the app is listening on port ${port}!`))
 
 const {Sequelize, Op, Model, DataTypes} = require('sequelize')
@@ -26,68 +29,61 @@ sequelize.authenticate().then(() => {
 
 //makes campuses model with the table 'campuses'
 const Campuses = sequelize.define('campuses', 
-                                {name: {type : Sequelize.TEXT,
-                                    allowNull : false},
-                                imageURL: {type:Sequelize.TEXT,
-                                    defaultValue : "no url"},
-                                address : {type:Sequelize.TEXT,
-                                    allowNull : false},
-                                description : {type : Sequelize.TEXT}
+                                {name: {
+                                    type : Sequelize.TEXT,
+                                    allowNull : false,
+                                },
+                                imageURL: {
+                                    type:Sequelize.TEXT,
+                                    defaultValue : "no url",
+                                },
+                                address : {
+                                    type:Sequelize.TEXT,
+                                    allowNull : false,
+                                },
+                                description : {
+                                    type : Sequelize.TEXT,
+                                }
                                 })
 
 
 //student module with table 'students'
 const Students = sequelize.define('students',
-                                {firstName: {type: Sequelize.TEXT,
-                                    allowNull: false,},
-                                lastName: {type: Sequelize.TEXT,
-                                    allowNull: false,},
-                                email: {type: Sequelize.TEXT,
+                                {firstName: {
+                                    type: Sequelize.TEXT,
+                                    allowNull: false,
+                                },
+                                lastName: {
+                                    type: Sequelize.TEXT,
+                                    allowNull: false,
+                                },
+                                email: {
+                                    type: Sequelize.TEXT,
                                     allowNull: false,
                                     validate: {
                                         isEmail : true
-                                    }},
-                                imageURL: {type:Sequelize.TEXT,
-                                    defaultValue : "no url"},
-                                gpa: {type: Sequelize.DOUBLE(),
-                                validate:{
-                                    min: 0,
-                                    max: 4,
-                                    }},
+                                    }
+                                },
+                                imageURL: {
+                                    type:Sequelize.TEXT,
+                                    defaultValue : "no url"
+                                },
+                                gpa: {
+                                    type: Sequelize.DOUBLE(),
+                                    validate:{
+                                        min: 0,
+                                        max: 4,
+                                    }
+                                },
+                                campusid:{
+                                    type: Sequelize.INTEGER,
+                                    allowNull : false,
+                                }
                                 })
-
-//sets the parameters, such as the table and it's contents to the database
-sequelize.sync({ force: true})
-    .then(()=>{
-        console.log(`Databse & tables created`)
-
-        Campuses.bulkCreate([
-            { name: 'Lucretia Marcigliano Campus', address : '50 P Avenue, Brooklyn, NY 11223'},
-            { name: 'Lafayette Educational Campus', address : '2630 Benson Ave, Brooklyn, NY 11214'},
-            { name: 'Central Michigan University Fort Hamilton Center', address : '218 Marshall Dr, Brooklyn, NY 11209'},
-            { name: 'Touro College, School for Lifelong Education', address : '1273 53rd St, Brooklyn, NY 11219'},
-        ]).then(function() {
-            return Campuses.findAll();
-        }).then(function(campuses) {
-            console.log(campuses)
-        })
-
-        Students.bulkCreate([
-            {firstName: 'Josh', lastName: 'Soromo', email: 'yahoo@gmail.com', gpa: 5,},
-            {firstName: 'Drake', lastName: 'Dano', email: 'drake@dano.com'},
-            {firstName: 'Pomo', lastName: 'Frodo', email: 'email@email.com'},
-            {firstName: 'Past', lastName: 'Future', email: 'present@email.com'},
-        ]).then(function(){
-            return Students.findAll();
-        }).then(function(students) {
-            console.log(students)
-        })
-
-    })
 
 //route for the home page
 app.get("/", (req, res) => {
-    res.send("hey")
+    res.send("nothing here")
 })
 
 //route for all campuses
@@ -100,20 +96,90 @@ app.get('/students', function(req, res) {
     Students.findAll().then(students => res.json(students))
 })
 
-//
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/campuses/:id', function(req, res){
-    Campuses.findAll({where : {id : req.params.id} }).then(campuses => res.json(campuses))
+//create new student through the post request
+app.post('/students', function(req, res) {
+    Students.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        campusid: req.body.campusid,
+        gpa: req.body.gpa,
+        imageURL : req.body.imageURL,
+    }).then(function(student) {
+        res.send(student)
+    }).catch(function(err) {
+        console.log(err)
+        res.send(err)
+    })
 })
+
+
+//create new campus through the post request
+app.post('/campuses', function(req, res) {
+    Campuses.create({
+        name: req.body.name,
+        address: req.body.address,
+        imageURL: req.body.imageURL,
+        description: req.body.description,
+    }).then(function(campus) {
+        res.send(campus)
+    }).catch(function(err) {
+        console.log(err)
+        res.send(err)
+    })
+})
+
+
+//delete student
+app.delete('/students/:id', function(req,res) {
+    Students.findByPk(req.params.id).then(function(student){
+        student.destroy()
+    }).then((student) => {
+        res.sendStatus(200)
+    }).catch(function(err) {
+        console.log(err)
+        res.send(err)
+    })
+})
+
+//delete campus
+app.delete('/campuses/:id', function(req,res) {
+    Campuses.findByPk(req.params.id).then(function(campus){
+        campus.destroy()
+    }).then((campus) => {
+        res.sendStatus(200)
+    }).catch(function(err) {
+        console.log(err)
+        res.send(err)
+    })
+})
+
+//find a student
+app.get('/students/:id', function(req, res) {
+    Students.findAll({where: {id: req.params.id}}).then(students => {
+        Campuses.findAll({where: {id: students[0].campusid}}).then(campuses => {
+            return res.json([].concat(students, campuses))
+        }).catch(function(err) { //if there is no campus linked to the student
+            res.json(students)
+        })
+    }).catch(function(err) {
+        console.log(err)
+        res.send(err)
+    })
+})
+
+//find a campus
+app.get('/campuses/:id', function(req, res) {
+    Campuses.findAll({where: {id: req.params.id}}).then(campuses => {
+        Students.findAll({where: {campusid: campuses[0].id}}).then(students => {
+            return res.json([].concat(campuses, students))
+        }).catch(function(err) { //if there are no students linked the campus
+            res.json(campuses)
+        })
+    }).catch(function(err) {
+        console.log(err)
+        res.send(err)
+    })
+})
+
 
